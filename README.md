337f95d3acb97c1d57f9f4acfe68353dd9fb1bd0
tneils218 <47965692+tneils218@users.noreply.github.com>
2023-10-04 00:02:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-qmck7c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-qmck7c)
---
+++ apis/request.ts
@@ -0,0 +1,69 @@
+import TokenManager, { injectBearer } from 'brainless-token-manager';
+import { extend } from 'umi-request';
+
+export const request = extend({
+  prefix: 'https://test-react.agiletech.vn',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  errorHandler: (error) => {
+    throw error?.data || error?.response;
+  },
+});
+
+const tokenManager = new TokenManager({
+  getAccessToken: async () => {
+    const token = localStorage.getItem('accessToken');
+
+    return `${token}`;
+  },
+  getRefreshToken: async () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    return `${refreshToken}`;
+  },
+  onInvalidRefreshToken: () => {
+    // Logout, redirect to login
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+  },
+  executeRefreshToken: async () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (!refreshToken) {
+      return {
+        token: '',
+        refresh_token: '',
+      };
+    }
+
+    const r = await request.post('/auth/refresh-token', {
+      data: {
+        refreshToken: refreshToken,
+      },
+    });
+
+    return {
+      token: r?.accessToken,
+      refresh_token: r?.refreshToken,
+    };
+  },
+  onRefreshTokenSuccess: ({ token, refresh_token }) => {
+    if (token && refresh_token) {
+      localStorage.setItem('accessToken', token);
+      localStorage.setItem('refreshToken', refresh_token);
+    }
+  },
+});
+
+export const privateRequest = async (
+  request: any,
+  suffixUrl: string,
+  configs?: any
+) => {
+  const token: string = configs?.token
+    ? configs?.token
+    : ((await tokenManager.getToken()) as string);
+
+  return request(suffixUrl, injectBearer(token, configs));
+};
